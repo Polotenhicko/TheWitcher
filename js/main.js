298c1b525ad951f5514dd2ce36f02d16c552f8a4
Polotenhicko <artem-gribanof@yandex.ru>
2021-07-21 14:19:54 +0300

    Подготовка

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-new Swiper('.swiper-container', {
-	loop: true,
-	navigation: {
-		nextEl: '.arrow',
-	},
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 20
+/*
+	new Swiper('.swiper-container', {
+		loop: true,
+		navigation: {
+			nextEl: '.arrow',
 		},
-		541: {
-			slidesPerView: 2,
-			spaceBetween: 40
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+			541: {
+				slidesPerView: 2,
+				spaceBetween: 40
+			}
 		}
-	}
-});
+	});
 
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.header');
-menuButton.addEventListener('click', function () {
-	menuButton.classList.toggle('menu-button-active');
-	menu.classList.toggle('header-active');
-})
+	const menuButton = document.querySelector('.menu-button');
+	const menu = document.querySelector('.header');
+	menuButton.addEventListener('click', function () {
+		menuButton.classList.toggle('menu-button-active');
+		menu.classList.toggle('header-active');
+	})
+*/
