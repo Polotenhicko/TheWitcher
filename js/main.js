ee6a418f2e8a206679395475981dbff89d1b00fc
Polotenhicko <artem-gribanof@yandex.ru>
2021-07-22 19:47:50 +0300

    1 урок

--- js/main.js
+++ js/main.js
@@ -23,12 +23,16 @@
 		menu.classList.toggle('header-active');
 	})
 */
-const getElement = (tagName, classNames) => {
+const getElement = (tagName, classNames, attributes) => {
 	const element = document.createElement(tagName);
 	if (classNames) {
-			element.classList.add(...classNames);
+		element.classList.add(...classNames);
 	}
-
+	if (attributes) {
+		for (const attribute in attributes) {
+			element[attribute] = attributes[attribute];
+		}
+	}
 	return element;
 };
 
@@ -36,6 +40,49 @@ const createHeader = (param) => {
 	const header = getElement('header');
 	const container = getElement('div', ['container']);
 	const wrapper = getElement('div', ['header']);
+
+	if (param.header.logo) {
+		const logo = getElement('img', ['logo'], {
+			src: param.header.logo,
+			alt: 'Логотип ' + param.title
+		});
+		wrapper.append(logo);
+	}
+
+	if (param.header.menu) {
+		const menuWrapper = getElement('nav', ['menu-list']);
+		const allMenu = param.header.menu.map(item => {
+			const menuLink = getElement('a', ['menu-link'], {
+				href: item.link,
+				textContent: item.title
+			});
+
+			//menuLink.textContent = item.title;
+			//menuLink.href = item.link;
+
+			return menuLink;
+		})
+		menuWrapper.append(...allMenu);
+		wrapper.append(menuWrapper);
+	}
+
+	if (param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title
+			}));
+
+			socialLink.href = item.link;
+
+			return socialLink;
+		});
+
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
+	}
 
 	header.append(container);
 	container.append(wrapper);
@@ -45,15 +92,51 @@ const createHeader = (param) => {
 
 const movieConstructor = (selector, options) => {
 	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+
+	if (options.title) {
+		document.title = options.title;
+	}
 
 	if (options.header) {
-			app.append(createHeader(options.header));
+			app.append(createHeader(options));
 	}
 };
 
 movieConstructor('.app', {
 	title: 'Ведьмак',
 	header: {
-			logo: ''
+			logo: 'witcher/logo.png',
+			social: [
+				{
+					title: 'Twitter',
+					link: 'https://twitter.com',
+					image: 'witcher/social/twitter.svg',
+				},
+				{
+					title: 'Instagram',
+					link: 'https://instagram.com',
+					image: 'witcher/social/instagram.svg',
+				},
+				{
+					title: 'Facebook',
+					link: 'https://facebook.com',
+					image: 'witcher/social/facebook.svg',
+				}
+			],
+			menu: [
+				{
+					title: 'Описание',
+					link: '#',
+				},
+				{
+					title: 'Трейлер',
+					link: '#',
+				},
+				{
+					title: 'Отзывы',
+					link: '#',
+				}
+			]
 	}
 });
